090a70acf2a9fa242989c56a4167b9929e485bb2
Jon George <jgeorge@byu.edu>
2014-12-11 08:43:27 -0700

    added function to send email to all registered users. Just needs a nice email template now

--- src/hn_follow/core/mailer.clj
+++ src/hn_follow/core/mailer.clj
@@ -4,15 +4,17 @@
             [postal.core :as postal]
             [hn-follow.core.account :as account]))
 
-
+;get an list of interaction feed objects from the last week
 (defn get-email-feed [username]
   (loop [feed []
          feed-list (map api/get-user (:follow (account/following username)))]
     (if (empty? feed-list)
-      (flatten feed)
+      (sort-by :time
+        (flatten feed))
       (recur (conj feed (api/week-feed (first feed-list))) (rest feed-list)))))
 
-(defn send-update [email username]
+;send and email with a email feed for the previous week's activity.
+(defn send-email-update [email username]
   (let [message (apply str (get-email-feed username))]
     (postal/send-message {:host "smtp.gmail.com"
                :user "hnfollow"
@@ -25,5 +27,13 @@
 
 ;(send-update "crewbie4life@gmail.com" "qzcx")
 
-
+(defn send-registered-users-update-email []
+  (loop [users ((account/all) :users)]
+    (let [next-user (first users)
+          email (:email (account/following next-user))]
+      (if (not (nil? email))
+        (println (str "update sent to " next-user "mailer response message: "
+         (send-update email next-user))))
+      (if (not(empty?(rest users)))
+        (recur (rest users))))))
 
