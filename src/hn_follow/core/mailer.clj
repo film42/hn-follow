574ad08fa80e083d8e57cc3cd72a3224e2df075f
Jon George <jgeorge@byu.edu>
2014-12-11 07:50:52 -0700

    mailer works at sending updates, just needs a function to parse accounts to get users with register email and send them all an update

--- src/hn_follow/core/mailer.clj
+++ src/hn_follow/core/mailer.clj
@@ -1,29 +1,29 @@
 ;Emails updates to subscribers.
 (ns hn-follow.core.mailer
-  (:require [postal.core :refer :all]))
+  (:require [hn-follow.core.api :as api]
+            [postal.core :as postal]
+            [hn-follow.core.account :as account]))
 
-(defn get-update [username last-update]
 
-  )
+(defn get-email-feed [username]
+  (loop [feed []
+         feed-list (map api/get-user (:follow (account/following username)))]
+    (if (empty? feed-list)
+      (flatten feed)
+      (recur (conj feed (api/week-feed (first feed-list))) (rest feed-list)))))
 
-(defn SendUpdate [email username last-update]
-  (let [message (get-update username last-update)]
-    (send-message {:host "smtp.gmail.com"
-               :user "crewbie4life"
-               :pass "XXX"
-               :ssl :yes!!!11}
-              {:from "crewbie4life@gmail.com"
-               :to ["film42@gmail.com"]
-               :subject "hn-follow update" ;What do we want to send as a subject?
-               :body message})
-    )
-  )
+(defn send-update [email username]
+  (let [message (apply str (get-email-feed username))]
+    (postal/send-message {:host "smtp.gmail.com"
+               :user "hnfollow"
+               :pass "hackernewsfollow"
+               :ssl true}
+              {:from "hnfollow@gmail.com"
+               :to [email]
+               :subject (str "hn-follow update for " username) ;What do we want to send as a subject?
+               :body message})))
 
-(send-message {:host "smtp.gmail.com"
-               :user "crewbie4life"
-               :pass "XXX"
-               :ssl :yes!!!11}
-              {:from "crewbie4life@gmail.com"
-               :to ["film42@gmail.com"]
-               :subject "postal.core email"
-               :body "Hey, so I sent you this with clojure ^.^"})
+;(send-update "crewbie4life@gmail.com" "qzcx")
+
+
+
